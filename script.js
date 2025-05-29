852fb7f28815c16c27cc429f563c98aff681585f
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-05-29 13:43:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 class Person {
   constructor(name, age) {
-    this.name = name;
-    this.age = age;
+    this._name = name;
+    this._age = age;
   }
 
   // Getter for name
   get name() {
-    return this.name;
+    return this._name;
   }
 
   // Getter for age
   get age() {
-    return this.age;
+    return this._age;
   }
 
   // Setter for age
   set age(newAge) {
-    this.age = newAge;
+    this._age = newAge;
   }
 }
 
 class Student extends Person {
   study() {
-    console.log(`${this.name} is studying`);
+    console.log(this._name+" is studying");
   }
 }
 
 class Teacher extends Person {
   teach() {
-    console.log(`${this.name} is teaching`);
+    console.log(this._name+" is teaching");
   }
 }
 
